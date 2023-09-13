b1769ad02462d99398e7ee129e4935bc9bf33116
Nuklles <103897836+Nuklles@users.noreply.github.com>
2023-09-13 19:50:22 -0300

    Update projetocarneirinhos.java

--- projetocarneirinhos.java
+++ projetocarneirinhos.java
@@ -5,7 +5,8 @@
 		//estrutura 1 
 		//for(int carneirinhos = 1 ; carneirinhos <=20 ; carneirinhos ++)
 		//	System.out.println(carneirinhos + " - Carneirinhos(s)");
-	
+	        // }
+		//}
 	//estrutura 2 
 		// o que importa é somente o bloco condicional
 		//int carneirinhos = 1;
@@ -16,3 +17,12 @@
 		//}
 
 		//}
+// estrutura 3
+// fazendo os número ficarem decrescentes
+// for(int carneirinhos = 20 ; carneirinhos >= 0 ; carneirinhos -- )
+// System.out.println(carneirinhos + " - Carneirinhos(s)");
+
+// }
+// }
+
+		
