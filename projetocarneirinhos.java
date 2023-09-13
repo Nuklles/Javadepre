d2da5725bf95b1042a851dc48a39718fd63540d8
Nuklles <103897836+Nuklles@users.noreply.github.com>
2023-09-13 19:41:12 -0300

    Create projetocarneirinhos.java

---
+++ projetocarneirinhos.java
@@ -0,0 +1,18 @@
+//public class teste {
+
+	//public static void main(String[] args) {
+		
+		//estrutura 1 
+		//for(int carneirinhos = 1 ; carneirinhos <=20 ; carneirinhos ++)
+		//	System.out.println(carneirinhos + " - Carneirinhos(s)");
+	
+	//estrutura 2 
+		// o que importa é somente o bloco condicional
+		//int carneirinhos = 1;
+		//for(; carneirinhos <= 20;) {
+		// System.out.println(carneirinhos +" - Carneirinhos(s)");
+		// carneirinhos ++;
+		// }
+		//}
+
+		//}
