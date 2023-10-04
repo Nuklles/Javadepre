b26951a5a931109faed60912e61ac4c3ef0c11fc
Nuklles <103897836+Nuklles@users.noreply.github.com>
2023-10-04 20:01:56 -0300

    Create ifelse.java

---
+++ ifelse.java
@@ -0,0 +1,17 @@
+package projeto;
+
+public class ifelse {
+
+	public static void main(String[] args) {
+
+		int x;
+		x = 20;
+		if (x < 10) {
+			System.out.println("X é menor que 10");
+		} else {
+			System.out.println("x é maior que 10");
+		}
+
+	}
+
+}
